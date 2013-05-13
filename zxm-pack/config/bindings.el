d4e676dcec8946544e23572129136819bac892e7
Martin <zxm@basecity.com>
2013-05-13 21:27:06 +1200

    my emacs 24 setup. base on emacs-live

---
+++ zxm-pack/config/bindings.el
@@ -0,0 +1,9 @@
+;; Place your bindings here.
+
+;; For example:
+(define-key global-map (kbd "C-+") 'text-scale-increase)
+(define-key global-map (kbd "C--") 'text-scale-decrease)
+(global-set-key (kbd "<C-S-up>")     'buf-move-up)
+(global-set-key (kbd "<C-S-down>")   'buf-move-down)
+(global-set-key (kbd "<C-S-left>")   'buf-move-left)
+(global-set-key (kbd "<C-S-right>")  'buf-move-right)
---
+++ zxm-pack/config/solarized-conf.el
@@ -0,0 +1,8 @@
+;; Solarized theme configuration file
+
+;;(live-add-pack-lib "solarized-emacs")
+;;(require 'solarized-theme)
+(live-add-pack-lib "emacs-color-theme-solarized")
+(require 'solarized-dark-theme)
+(load-theme 'solarized-dark t)
+
