93ff0130da96a2aee0aff2be95ba059ef02465c7
Martin <zxm@basecity.com>
2013-05-13 21:50:07 +1200

    only modify chinese font.

--- zxm-pack/init.el
+++ zxm-pack/init.el
@@ -8,12 +8,10 @@
 (live-load-config-file "solarized-conf.el")
 (live-load-config-file "clean-mode-line.el")
 (set-face-attribute 'vertical-border nil :foreground "red")
+
 (create-fontset-from-fontset-spec
 "-apple-bitstream vera sans mono-medium-r-normal--12-*-*-*-*-*-fontset-mymonaco,
-ascii:-apple-Monaco-medium-normal-normal-*-12-*-*-*-m-0-iso10646-1,
-chinese-gb2312:-apple-STHeiti-medium-normal-normal-12-*-*-*-*-p-0-iso10646-1,
-latin-iso8859-1:-apple-Monaco-medium-normal-normal-*-12-*-*-*-m-0-iso10646-1,
-mule-unicode-0100-24ff:-apple-Monaco-medium-normal-normal-*-12-*-*-*-m-0-iso10646-1")
+chinese-gb2312:-apple-STHeiti-medium-normal-normal-12-*-*-*-*-p-0-iso10646-1")
 
 (setq default-frame-alist (append '((font . "fontset-mymonaco")) default-frame-alist))
 (set-default-font "fontset-mymonaco")
